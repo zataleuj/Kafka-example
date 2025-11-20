d2337ff3e6013d48dcf254295cd9da13ed962614
zataleuj <zataleuj@gmail.com>
2025-11-21 01:11:46 +0530

    initial commit

---
+++ producer/src/main/java/com/demo/producer/KafkaProducer.java
@@ -0,0 +1,25 @@
+package com.demo.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class KafkaProducer {
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/sent")
+    public String setMessage(@RequestParam String message){
+        kafkaTemplate.send("my-topic",message);
+        return "message sent :"+message;
+    }
+}
